6409af233cbf3aca9d7ca09b3c7ed54c6cf56908
rsoto23 <rsoto23@cathedral-elpaso.org>
2022-11-03 23:03:29 -0600

    Possibly Fixed RTP and CR Servos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CR_Servo.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+
+@TeleOp
+
+public class CR_Servo extends LinearOpMode {
+    private CRServo servoIntake;
+
+    @Override
+    public void runOpMode() {
+    servoIntake = hardwareMap.get(CRServo.class,"servoIntake");
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (d
+        // .
+        // river presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //MECHANISM CODE
+
+           if (gamepad2.right_trigger>=.3 && gamepad2.left_trigger <=.3 ) {
+           servoIntake.setPower(1);
+           }
+            else if (gamepad2.right_trigger<.3 && gamepad2.left_trigger >.3 ) {
+                servoIntake.setPower(0);
+            }
+            else{
+                servoIntake.setPower(0.5);
+            }
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RTP_TEST.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+
+public class RTP_TEST extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+        DcMotor motorLeftLift = hardwareMap.dcMotor.get("motorLeftLift");
+        DcMotor motorRightLift = hardwareMap.dcMotor.get("motorLeftLift");
+
+
+        motorLeftLift.setTargetPosition(0);
+        motorRightLift.setTargetPosition(0);
+        motorLeftLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        int ArmTarget = 0;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //MECHANISM CODE
+//134.4 ticks per rotation
+// max extension at 8.7 * 134.4
+
+
+            if (gamepad2.left_bumper) {
+                ArmTarget = 100; //intaking
+            }
+            else if (gamepad2.dpad_down) {
+                ArmTarget = 200; //Low level
+            }
+            else if (gamepad2.dpad_up) {
+                ArmTarget = 300; //Mid level
+            }
+            else if (gamepad2.right_bumper) {
+                ArmTarget = 400; //High level
+            }
+
+            //stuff for arm position control
+            motorLeftLift.setTargetPosition(ArmTarget);
+            motorRightLift.setTargetPosition(ArmTarget);
+            motorLeftLift.setPower(.75);
+            motorRightLift.setPower(.75);
+
+            telemetry.addData("Left Lift Position", motorLeftLift.getCurrentPosition());
+            telemetry.addData("Right Lift Position", motorRightLift.getCurrentPosition());
+
+            telemetry.update();
+        }
+    }
+}
