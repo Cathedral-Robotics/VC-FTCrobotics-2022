35edba65edebfefe51e8d9e0c04b6f62b665ea2d
eortega02 <ethan.ortm.18@gmail.com>
2022-11-14 14:07:33 -0700

    added ethantest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ethantest.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Autonomous
+
+public class ethantest extends LinearOpMode {
+    private CRServo servoIntake;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        DcMotor motorLeftLift = hardwareMap.dcMotor.get("motorLeftLift");
+        DcMotor motorRightLift = hardwareMap.dcMotor.get("motorRightLift");
+
+        servoIntake = hardwareMap.get(CRServo.class,"servoIntake");
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorLeftLift.setTargetPosition(0);
+        motorRightLift.setTargetPosition(0);
+        motorLeftLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        int ArmTarget = 0;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (d
+        // .
+        // river presses PLAY)
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            //stuff for arm position control
+            motorLeftLift.setTargetPosition(-1*ArmTarget);
+            motorRightLift.setTargetPosition(ArmTarget);
+            motorLeftLift.setPower(.75);
+            motorRightLift.setPower(.75);
+
+            telemetry.addData("Left Lift Position", motorLeftLift.getCurrentPosition());
+            telemetry.addData("Right Lift Position", motorRightLift.getCurrentPosition());
+            telemetry.update();
+
+
+            motorFrontLeft.setPower(-.25);
+            motorBackLeft.setPower(.25);
+            motorFrontRight.setPower(.25);
+            motorBackRight.setPower(-.25);
+            sleep(3500);
+
+            motorFrontLeft.setPower(0);
+            motorBackLeft.setPower(0);
+            motorFrontRight.setPower(0);
+            motorBackRight.setPower(0);
+            sleep(50);
+
+            motorFrontLeft.setPower(.25);
+            motorBackLeft.setPower(.25);
+            motorFrontRight.setPower(.25);
+            motorBackRight.setPower(.25);
+            sleep(3500);
+
+            motorFrontLeft.setPower(0);
+            motorBackLeft.setPower(0);
+            motorFrontRight.setPower(0);
+            motorBackRight.setPower(0);
+            sleep(5000000);
+
+            //stops motor after no input
+            motorFrontLeft.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorBackLeft.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorFrontRight.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorBackRight.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+    }
+}
