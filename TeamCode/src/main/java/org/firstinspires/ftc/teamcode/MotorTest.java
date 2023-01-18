24d4f290a1e7e6897f6b06eab01afb7802383344
rsoto23 <rsoto23@cathedral-elpaso.org>
2023-01-17 21:53:38 -0700

    Added MotorTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@TeleOp
+public class MotorTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+
+        /* Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+*/
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (d
+        // .
+        // river presses PLAY)
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.dpad_up) {
+                motorFrontLeft.setPower(1);
+            }
+            if (gamepad1.dpad_down){
+                motorBackLeft.setPower(1);
+            }
+            if (gamepad1.x){
+                motorFrontRight.setPower(1);
+            }
+            if (gamepad1.a){
+                motorBackRight.setPower(1);
+            }
+
+            telemetry.update();
+
+
+            //stops motor after no input
+            motorFrontLeft.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorBackLeft.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorFrontRight.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+            motorBackRight.setZeroPowerBehavior (DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+    }
+}
